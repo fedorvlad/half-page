3ce6091e9f6d22e7dbde6fc1b442f7858cf4c063
Volodymyr Fedorak <fedorvlad@yahoo.com>
2018-11-01 00:08:33 +0200

    update func validate ``

--- js/myApp.js
+++ js/myApp.js
@@ -1,3 +1,4 @@
+
 const serverURL = 'https://fedorvlad.github.io/half-page/';
 var galeryHtml = '';
 
@@ -14,32 +15,33 @@ $(document).ready(
 
 // Validate Send Message
 console.log('message before func' );
+
 function validateForm() {
-    var x = document.getElementById('name').value;
+    var x = document.getElementById(`name`).value;
     console.log('value of X_' +x+'_' );
     if (x == "") {
-        document.getElementById('status').innerHTML = "Name cannot be empty";
+        document.getElementById(`status`).innerHTML = "Name cannot be empty";
         return false;
     }
-    x = document.getElementById('email').value;
+    x = document.getElementById(`email`).value;
     if (x == "") {
-        document.getElementById('status').innerHTML = "Email cannot be empty";
+        document.getElementById(`status`).innerHTML = "Email cannot be empty";
         return false;
     } else {
         var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
         if (!re.test(x)) {
-            document.getElementById('status').innerHTML = "Email format invalid";
+            document.getElementById(`status`).innerHTML = "Email format invalid";
             return false;
         }
     }
-    x = document.getElementById('subject').value;
+    x = document.getElementById(`subject`).value;
     if (x == "") {
         document.getElementById('status').innerHTML = "Subject cannot be empty";
         return false;
     }
-    x = document.getElementById('message').value;
+    x = document.getElementById(`message`).value;
     if (x == "") {
-        document.getElementById('status').innerHTML = "Message cannot be empty";
+        document.getElementById(`status`).innerHTML = "Message cannot be empty";
         return false;
     }
     //get input field values data to be sent to server
